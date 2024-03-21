8988e2ef0494645ebc9aa4e4df72ceaa44c07156
PrinceCharles47 <clemente.pcharles@gmail.com>
2024-03-21 16:43:55 +0800

    navigation drawer

--- src/edventure_quest_frontend/src/App.jsx
+++ src/edventure_quest_frontend/src/App.jsx
@@ -1,31 +1,53 @@
 import { useState } from 'react';
 import { edventure_quest_backend } from 'declarations/edventure_quest_backend';
 
+import "./App.css"
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider";
+import routes from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, alpha, getContrastRatio } from "@mui/material/styles";
+import colors from "./utils/colors";
+import UserProvider from "./providers/UserProvider";
+
+const blueBase = colors.primary;
+const blueMain = colors.primary;
+
+const blueLightBase = colors.secondary;
+const blueLightMain = colors.secondary;
+let mode = "dark";
+
+const theme = createTheme({
+  palette: {
+    mode: mode,
+    blue: {
+      main: blueMain,
+      light: alpha(blueBase, 0.5),
+      dark: alpha(blueBase, 0.9),
+      contrastText:
+        getContrastRatio(blueMain, "#fff") > 4.5 ? "#fff" : "#111",
+    },
+    blueLight: {
+      main: blueLightMain,
+      light: alpha(blueLightBase, 0.5),
+      dark: alpha(blueLightBase, 0.9),
+      contrastText:
+        getContrastRatio(blueLightMain, "#fff") > 4.5 ? "#fff" : "#111",
+    },
+  },
+});
+
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    edventure_quest_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <AuthProvider>
+      <UserProvider>
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={routes} />
+        </ThemeProvider>
+      </UserProvider>
+    </AuthProvider>
   );
 }
 
 export default App;
+
